8d92fa4d3567b826829e27eaacd57630f90160b3
TanishqSingla <tanishqsingla00@gmail.com>
2022-10-18 15:03:23 +0530

    Added theme toggle

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,34 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$, useClientEffectQrl, useStore } from "@builder.io/qwik";
 
 export default component$(() => {
+	const themes = ["dark", "light"];
+
+	const store = useStore({ themeIndex: 0 });
+
+	const toggleTheme = $(() => {
+		// I WANTED TO TRY SOMETHING SO LET ME BE
+		localStorage.theme = themes[store.themeIndex];
+		if (
+			localStorage.theme === "dark" ||
+			(!("theme" in localStorage) &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches)
+		) {
+			document.documentElement.classList.add("dark");
+		} else {
+			document.documentElement.classList.remove("dark");
+			store.themeIndex = 1;
+		}
+	});
+
+	useClientEffectQrl(toggleTheme);
+
 	return (
-		<header class='sticky bg-gray-100/40 backdrop-blur-sm px-4 py-4 mt-4 rounded-xl'>
+		<header class="sticky bg-gray-100/40 backdrop-blur-sm px-4 py-4 mt-4 rounded-xl">
 			<nav class="flex">
 				<ul class="flex gap-8 grow">
-          <li>
-            <a href="/"></a>
-          </li>
+					<li>
+						<a href="/"></a>
+					</li>
 					<li>
 						<a href="">Projects</a>
 					</li>
@@ -19,7 +40,14 @@ export default component$(() => {
 					</li>
 				</ul>
 				<div class="theme-switcher">
-					<button>theme</button>
+					<button
+						onClick$={() => {
+							store.themeIndex = store.themeIndex ^ 1;
+              toggleTheme();
+						}}
+					>
+						theme
+					</button>
 				</div>
 			</nav>
 		</header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,14 @@
 
 module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class',
 	theme: {
 		extend: {
 			colors: {
 				primary: "#344E41",
 				secondary: "#A3B18A",
 				surface: "#DAD7CD",
+        'dark-surface': '#212121',
         highlight: '#588157'
 			},
 		},
